584ec94f92dc3198b6323420672dbc403059771e
HZK <hzkterry@hotmail.com>
2021-01-26 04:40:50 +0900

    new

---
+++ Assets/Scripts/RoomGenerator.cs
@@ -0,0 +1,207 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class RoomGenerator : MonoBehaviour
+{
+    public enum Direction { up, down, left, right };
+    public Direction direction;
+
+    [Header("Room Info")]
+    public GameObject roomPrefab;
+    public int roomNumber;
+    public Color startColour, endColour;
+    private GameObject endRoom;
+
+    [Header("Position Control")]
+    public Transform generatorPoint;
+    public float xOffset;
+    public float yOffset;
+    public LayerMask roomLayer;
+    public int maxStep;
+
+    //public List<GameObject> rooms = new List<GameObject>();
+    public List<Room> rooms = new List<Room>();
+
+    List<GameObject> farRooms = new List<GameObject>();
+    List<GameObject> lessFarRooms = new List<GameObject>();
+    List<GameObject> oneWayRooms = new List<GameObject>();
+
+    public WallType wallType;
+
+    void Start()
+    {
+        for (int i = 0; i < roomNumber; i++)
+        {
+            rooms.Add(Instantiate(roomPrefab, generatorPoint.position, Quaternion.identity).GetComponent<Room>());
+
+            //改变point位置
+            ChangePointPos();
+        }
+
+        rooms[0].GetComponent<SpriteRenderer>().color = startColour;//改变第1个房间的颜色
+        //rooms[roomNumber-1].GetComponent<SpriteRenderer>().color = endColour;
+
+        endRoom = rooms[0].gameObject;
+
+        foreach (var room in rooms)
+        {
+            //if (room.transform.position.sqrMagnitude > endRoom.transform.position.sqrMagnitude)
+            //{
+            //    endRoom = room.gameObject;
+            //}
+
+            SetupRoom(room, room.transform.position);
+        }
+
+        FindEndRoom();
+
+        endRoom.GetComponent<SpriteRenderer>().color = endColour;
+    }
+
+    void Update()
+    {
+        //if (Input.anyKeyDown)
+        //{
+        //    SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        //}
+    }
+
+    public void ChangePointPos()
+    {
+        do
+        {
+            direction = (Direction)Random.Range(0, 4);
+
+            switch (direction)
+            {
+                case Direction.up:
+                    generatorPoint.position += new Vector3(0, yOffset, 0);
+                    break;
+                case Direction.down:
+                    generatorPoint.position += new Vector3(0, -yOffset, 0);
+                    break;
+                case Direction.left:
+                    generatorPoint.position += new Vector3(-xOffset, 0, 0);
+                    break;
+                case Direction.right:
+                    generatorPoint.position += new Vector3(xOffset, 0, 0);
+                    break;
+            }
+
+        } while (Physics2D.OverlapCircle(generatorPoint.position, 0.2f, roomLayer));
+    }
+
+    public void SetupRoom(Room newRoom, Vector3 roomPosition)
+    {
+        //检查碰撞体是否位于一个圆形区域内,判断上下左右是否有其他房间
+        newRoom.roomUp = Physics2D.OverlapCircle(
+            roomPosition + new Vector3(0, yOffset, 0), 0.2f, roomLayer);
+
+        newRoom.roomDown = Physics2D.OverlapCircle(
+            roomPosition + new Vector3(0, -yOffset, 0), 0.2f, roomLayer);
+
+        newRoom.roomLeft = Physics2D.OverlapCircle(
+            roomPosition + new Vector3(-xOffset, 0, 0), 0.2f, roomLayer);
+
+        newRoom.roomRight = Physics2D.OverlapCircle(
+            roomPosition + new Vector3(xOffset, 0, 0), 0.2f, roomLayer);
+
+        newRoom.UpdateRoom(xOffset,yOffset);//参考上面Room脚本的内容
+
+        switch (newRoom.doorNumber)
+        {
+            case 1:
+                if (newRoom.roomUp)
+                    Instantiate(wallType.singleUp, roomPosition, Quaternion.identity);
+                if (newRoom.roomDown)
+                    Instantiate(wallType.singleBottom, roomPosition, Quaternion.identity);
+                if (newRoom.roomLeft)
+                    Instantiate(wallType.singleLeft, roomPosition, Quaternion.identity);
+                if (newRoom.roomRight)
+                    Instantiate(wallType.singleRight, roomPosition, Quaternion.identity);
+                break;
+
+            case 2:
+                if (newRoom.roomLeft && newRoom.roomUp)
+                    Instantiate(wallType.doubleLU, roomPosition, Quaternion.identity);
+                if (newRoom.roomLeft && newRoom.roomRight)
+                    Instantiate(wallType.doubleLR, roomPosition, Quaternion.identity);
+                if (newRoom.roomLeft && newRoom.roomDown)
+                    Instantiate(wallType.doubleLB, roomPosition, Quaternion.identity);
+                if (newRoom.roomUp && newRoom.roomRight)
+                    Instantiate(wallType.doubleUR, roomPosition, Quaternion.identity);
+                if (newRoom.roomUp && newRoom.roomDown)
+                    Instantiate(wallType.doubleUB, roomPosition, Quaternion.identity);
+                if (newRoom.roomRight && newRoom.roomDown)
+                    Instantiate(wallType.doubleRB, roomPosition, Quaternion.identity);
+                break;
+
+            case 3:
+                if (newRoom.roomLeft && newRoom.roomUp && newRoom.roomRight)
+                    Instantiate(wallType.tripleLUR, roomPosition, Quaternion.identity);
+                if (newRoom.roomLeft && newRoom.roomRight && newRoom.roomDown)
+                    Instantiate(wallType.tripleLRB, roomPosition, Quaternion.identity);
+                if (newRoom.roomDown && newRoom.roomUp && newRoom.roomRight)
+                    Instantiate(wallType.tripleURB, roomPosition, Quaternion.identity);
+                if (newRoom.roomLeft && newRoom.roomUp && newRoom.roomDown)
+                    Instantiate(wallType.tripleLUB, roomPosition, Quaternion.identity);
+                break;
+
+            case 4:
+                if (newRoom.roomLeft && newRoom.roomUp && newRoom.roomRight && newRoom.roomDown)
+                    Instantiate(wallType.fourDoors, roomPosition, Quaternion.identity);
+                break;
+        }
+    }
+
+    public void FindEndRoom()
+    {
+        //最大数值 最远距离数字
+        for (int i = 0; i < rooms.Count; i++)
+        {
+            if (rooms[i].stepToStart > maxStep)
+                maxStep = rooms[i].stepToStart;
+        }
+
+        //获得最远房间和第二远
+        foreach (var room in rooms)
+        {
+            if (room.stepToStart == maxStep)
+                farRooms.Add(room.gameObject);
+
+            if (room.stepToStart == maxStep - 1)
+                lessFarRooms.Add(room.gameObject);
+        }
+
+        for (int i = 0; i < farRooms.Count; i++)
+        {
+            if (farRooms[i].GetComponent<Room>().doorNumber == 1)
+                oneWayRooms.Add(farRooms[i]);//最远房间里的单侧门加入
+        }
+
+        for (int i = 0; i < lessFarRooms.Count; i++)
+        {
+            if (lessFarRooms[i].GetComponent<Room>().doorNumber == 1)
+                oneWayRooms.Add(lessFarRooms[i]);//第二远房间里的单侧门加入
+        }
+
+        if (oneWayRooms.Count != 0)
+        {
+            endRoom = oneWayRooms[Random.Range(0, oneWayRooms.Count)];
+        }
+        else
+        {
+            endRoom = farRooms[Random.Range(0, farRooms.Count)];
+        }
+    }
+}
+
+[System.Serializable]
+public class WallType
+{
+    public GameObject singleLeft, singleRight, singleUp, singleBottom, 
+        doubleLU, doubleLR, doubleLB, doubleUR, doubleUB, doubleRB, 
+        tripleLUR, tripleLUB, tripleURB, tripleLRB, fourDoors;
+}
